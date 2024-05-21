19e6f473cc059d9ffff02da8389ffef8ca686419
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-21 19:00:03 +0200

    resolved /about, and layout

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 import { Navbar } from "../components";
 const AboutPage = () => {
   return (
-    <>
+    <div style={{ overflowY: 'none' }}>
       <Navbar />
       <div className="container mx-8 my-3 py-3">
         <h1 className="text-center">About Us</h1>
         <hr />
-        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: "column",  margin: "0 auto", gap: "32px" }}>
-          <h2>Welcome to New Tech</h2>
-          <p className="" style={{ textAlign: 'center', maxWidth: '48rem', fontSize: '1.2rem',  margin: "0 auto"}}>
+        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: "column",  margin: "0 auto", gap: "32px", paddingTop: "25px" }}>
+          <h3 style={{ textAlign: 'center'}}>Welcome to New Tech</h3>
+          <p className="" style={{ textAlign: 'center', maxWidth: '48rem', fontSize: '1.1rem',  margin: "0 auto"}}>
           At New Tech, we're passionate about harnessing the power of technology to drive innovation and progress. Our mission is to provide cutting-edge solutions, expert insights, and top-notch products to help individuals and businesses thrive in a rapidly evolving digital landscape.
           </p>
 
-          <h2>Our Story</h2>
-          <p className="" style={{ textAlign: 'center', maxWidth: '48rem', fontSize: '1.2rem',  margin: "0 auto"}}>
+          <h3 style={{ textAlign: 'center'}}>Our Story</h3>
+          <p className="" style={{ textAlign: 'center', maxWidth: '48rem', fontSize: '1.1rem',  margin: "0 auto"}}>
           Founded in 2022 New Tech has grown into a trusted hub for all things tech. We're dedicated to staying at the forefront of technological advancements, ensuring our customers receive the best guidance and support possible.
           </p>
           
-          <h2>Our Values</h2>
-          <p className="" style={{ textAlign: 'center', maxWidth: '48rem', fontSize: '1.2rem',  margin: "0 auto"}}>
-          <ul>
-            <li>Innovation: We embrace the latest technologies and ideas to stay ahead of the curve.</li>
-            <li>Excellence: We strive for exceptional quality in everything we do.</li>
-            <li>Customer-centric: Your success is our top priority.</li>
-            <li>Collaboration: We work together to achieve great things.</li>
+          <h3 style={{ textAlign: 'center'}}>Our Values</h3>
+          <p className="" style={{ textAlign: 'center', maxWidth: '48rem', fontSize: '1.1rem',  margin: "0 auto"}}>
+          <ul style={{ textAlign: 'left' }}>
+            <li><strong>Innovation</strong>: We embrace the latest technologies and ideas to stay ahead of the curve.</li>
+            <li><strong>Excellence</strong>: We strive for exceptional quality in everything we do.</li>
+            <li><strong>Customer-centric</strong>: Your success is our top priority.</li>
+            <li><strong>Collaboration</strong>: We work together to achieve great things.</li>
           </ul>
           </p>
 
-          <p style={{ textAlign: 'center', fontWeight: 'bold' , maxWidth: '48rem', fontSize: '1.2rem',  margin: "0 auto"}}>
+          <p style={{ textAlign: 'center', fontWeight: 'bold' , maxWidth: '48rem', fontSize: '1rem',  margin: "0 auto", paddingTop: "18px" }}>
           Join the New Tech community today and discover a world of possibilities!
           </p>
 
@@ -79,7 +79,7 @@ const AboutPage = () => {
         {/* ---- products end ---- */}
 
       </div>
-    </>
+    </div>
   )
 }
 
